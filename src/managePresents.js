588869a249bb19f3e0714b74147208bf5c814f17
Precious <preciousmaregwa1997@gmail.com>
2022-07-28 19:12:43 +0300

    commit tree a029ffc429ac9fe5df1162e71d19df85646d8296

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,10 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  if (action.type==="presents/increase"){
+    return {numberOfPresents:state.numberOfPresents+1}
+  }
+  else{
+    return state;
+  }
 }
